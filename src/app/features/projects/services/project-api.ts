60469e2678668326ae1491e98494690f5bef425b
JonathanGea <78746871+JonathanGea@users.noreply.github.com>
2025-07-20 22:57:16 +0700

    update desain

--- src/app/features/projects/services/project-api.ts
+++ src/app/features/projects/services/project-api.ts
@@ -13,6 +13,42 @@ export class ProjectsService {
                 ],
                 "livePreviewUrl": "https://perfume-ecommerce-web-fe-public-production.up.railway.app/h",
                 "currentIndex": 0
+            },
+            {
+                "title": "Katalog Makanan Online (Order via WhatsApp)",
+                "description": "Platform pemesanan makanan dengan desain responsif yang terintegrasi langsung ke WhatsApp untuk proses order yang lebih efisien.",
+                "images": [
+                    "https://imageeweb.s3.us-east-1.amazonaws.com/jonathangea.dev/food%3Dorder-app.webp"
+                ],
+                "livePreviewUrl": "https://perfume-ecommerce-web-fe-public-production.up.railway.app/h",
+                "currentIndex": 0
+            },
+            {
+                "title": "Katalog Makanan Online (Order via WhatsApp)",
+                "description": "Platform pemesanan makanan dengan desain responsif yang terintegrasi langsung ke WhatsApp untuk proses order yang lebih efisien.",
+                "images": [
+                    "https://imageeweb.s3.us-east-1.amazonaws.com/jonathangea.dev/food%3Dorder-app.webp"
+                ],
+                "livePreviewUrl": "https://perfume-ecommerce-web-fe-public-production.up.railway.app/h",
+                "currentIndex": 0
+            },
+            {
+                "title": "Katalog Makanan Online (Order via WhatsApp)",
+                "description": "Platform pemesanan makanan dengan desain responsif yang terintegrasi langsung ke WhatsApp untuk proses order yang lebih efisien.",
+                "images": [
+                    "https://imageeweb.s3.us-east-1.amazonaws.com/jonathangea.dev/food%3Dorder-app.webp"
+                ],
+                "livePreviewUrl": "https://perfume-ecommerce-web-fe-public-production.up.railway.app/h",
+                "currentIndex": 0
+            },
+            {
+                "title": "Katalog Makanan Online (Order via WhatsApp)",
+                "description": "Platform pemesanan makanan dengan desain responsif yang terintegrasi langsung ke WhatsApp untuk proses order yang lebih efisien.",
+                "images": [
+                    "https://imageeweb.s3.us-east-1.amazonaws.com/jonathangea.dev/food%3Dorder-app.webp"
+                ],
+                "livePreviewUrl": "https://perfume-ecommerce-web-fe-public-production.up.railway.app/h",
+                "currentIndex": 0
             }
         ];
     }
